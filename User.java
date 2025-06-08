34da2c21d05cb4cc9623e71bf6f3a217085da272
kaylabiwott <biwottkayla@gmail.com>
2025-06-08 20:13:04 +0300

    Create User.java
    Add Java example that violates Single Responsibility Principle

---
+++ User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void saveToDatabase() {
+        System.out.println("Saving user to the database");
+    }
+
+    public void sendEmail() {
+        System.out.println("Sending email to user");
+    }
+}
